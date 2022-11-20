51f7f6a60dee3edd76dc76bede37a53584ecd09f
Dominik <droko@gmx.de>
2022-11-20 01:20:19 +0100

    fixes in Reimerei + own examples

--- Junior1/main.py
+++ Junior1/main.py
@@ -1,5 +1,5 @@
 import re
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 VOWELS = "aeiouäöü"
 
@@ -9,11 +9,11 @@ def find_rhyme_pairs(word_list: List[str]) -> List[Tuple[str, str]]:
 
     for idx, word_1 in enumerate(word_list):
         for word_2 in word_list[idx + 1:]:
-            if word_1.endswith(word_2) or word_2.endswith(word_1):
+            if word_1.casefold().endswith(word_2.casefold()) or word_2.casefold().endswith(word_1.casefold()):
                 continue
 
-            word_1_rhyme_end = get_rhyme_part(word_1)
-            word_2_rhyme_end = get_rhyme_part(word_2)
+            word_1_rhyme_end = get_rhyme_part(word_1.casefold())
+            word_2_rhyme_end = get_rhyme_part(word_2.casefold())
 
             if word_1_rhyme_end and word_2_rhyme_end and word_1_rhyme_end == word_2_rhyme_end:
                 rhyme_pairs.append((word_1, word_2))
@@ -21,32 +21,60 @@ def find_rhyme_pairs(word_list: List[str]) -> List[Tuple[str, str]]:
     return rhyme_pairs
 
 
-def get_rhyme_part(word: str) -> str:
-    at_last = False
-    had_consonant_after_last = False
-    at_next_to_last = False
+def get_rhyme_part(word: str) -> Optional[str]:
+    """
+    This function iterates over the word backwards.
+    It searches for the first vowel group,
+    then for the second vowel group (there must at least one consonant between vowel groups)
+    and returns the part of the word that is from the second vowel group to the end of the word.
+    If there is only one vowel group in the whole word, it returns after finding the first vowel group.
+
+
+    The function return None if any of the following is true:
+    - the word is empty
+    - the word has no vowel groups
+    - the resulting part of the word is less than half the whole word (violates second rule of task)
+
+    :param word: The word to get the rhyme part from
+    :return: The rhyme part of the word as string or None if [check list above]
+    """
+    reached_last_vowel_group = False
+    reached_consonant_after_last_vowel_group = False
+    reached_second_vowel_group = False
     result = None
     for idx, i in enumerate(reversed(word)):
-        if i in VOWELS:
-            if not at_last and not any([j in VOWELS for j in word[:-(idx + 1)]]):
+        if is_vowel(i, word):
+            if not any([is_vowel(j, word) for j in word[:-(idx + 1)]]):
                 result = word[-(idx + 1):]
                 break
-            if at_last and had_consonant_after_last:
-                at_next_to_last = True
-            at_last = True
-        elif at_next_to_last:
+            if reached_last_vowel_group and reached_consonant_after_last_vowel_group:
+                reached_second_vowel_group = True
+            reached_last_vowel_group = True
+        elif reached_second_vowel_group:
             result = word[-idx:]
             break
-        elif at_last and not had_consonant_after_last:
-            had_consonant_after_last = True
-    if (len(word) / 2) > len(result):
+        elif reached_last_vowel_group and not reached_consonant_after_last_vowel_group:
+            reached_consonant_after_last_vowel_group = True
+    if not result or (len(word) / 2) > len(result):
         result = None
     return result
 
 
+def is_vowel(char: str, word: str) -> bool:
+    # If the char is "y" and no other vowel is in the word, "y" is considered a vowel
+    if char == "y" and not any(v in word for v in VOWELS):
+        return True
+    return char in VOWELS
+
+
 def main():
-    word_list = open("reimerei0.txt", encoding="utf-8").read().splitlines()[:-1]
+    word_list = open("reimerei0.txt", encoding="utf-8").read().splitlines()
     pairs = find_rhyme_pairs(word_list)
+
+    if not pairs:
+        print("Keine Reimpaare gefunden.")
+        return
+
     print("Alle Reimpaare:")
     for pair in pairs:
         print(f"{pair[0]} : {pair[1]}")
